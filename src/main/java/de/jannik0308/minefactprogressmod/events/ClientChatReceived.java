560697bc77ca3a11426523ec33a1c377f5f31ae2
Jannik0308 <jnkarger01@gmail.com>
2021-06-24 22:03:47 +0200

    Added Discord Webhook support

--- src/main/java/de/jannik0308/minefactprogressmod/events/ClientChatReceived.java
+++ src/main/java/de/jannik0308/minefactprogressmod/events/ClientChatReceived.java
@@ -1,9 +1,13 @@
 package de.jannik0308.minefactprogressmod.events;
 
 import de.jannik0308.minefactprogressmod.MineFactProgressMod;
+import de.jannik0308.minefactprogressmod.utils.DiscordWebhook;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class ClientChatReceived {
 
@@ -20,11 +24,18 @@ public class ClientChatReceived {
                 int count = Integer.parseInt(countStr);
                 MineFactProgressMod.LOGGER.info(count);
                 setProjects(count);
-            } catch (NumberFormatException ignored) {}
+            } catch (NumberFormatException | IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
-    private void setProjects(int projects) {
-
+    private void setProjects(int projects) throws IOException {
+        DiscordWebhook webhook = new DiscordWebhook("https://discord.com/api/webhooks/857708669571170344/-KhCDHtUp6ECZAtumEjsUiUlf6sg8DsmYCq_dbM047ifRpENjOwroOPGWvzhh8jGVEYn");
+        webhook.addEmbed(new DiscordWebhook.EmbedObject()
+            .setTitle("Current Project Count")
+            .setDescription("/projects list got executed and the current project count is **" + projects + "**!")
+            .setColor(Color.CYAN));
+        webhook.execute();
     }
 }
