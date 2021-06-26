0d285ceeefceb3737fcbba3a9521bbcd1fb155c7
Jannik0308 <jnkarger01@gmail.com>
2021-06-26 22:06:26 +0200

    Changed User-Agent to Minefact-NewYork-Progress

--- src/main/java/de/jannik0308/minefactprogressmod/utils/DiscordWebhook.java
+++ src/main/java/de/jannik0308/minefactprogressmod/utils/DiscordWebhook.java
@@ -129,7 +129,7 @@ public class DiscordWebhook {
         URL url = new URL(this.url);
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         connection.addRequestProperty("Content-Type", "application/json");
-        connection.addRequestProperty("User-Agent", "Java-DiscordWebhook-BY-Gelox_");
+        connection.addRequestProperty("User-Agent", "Minefact-NewYork-Progress");
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
 
