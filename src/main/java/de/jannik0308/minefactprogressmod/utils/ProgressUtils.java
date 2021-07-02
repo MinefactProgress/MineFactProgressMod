1089ed85edad4c44d1cb38197b06e8d4daa60075
Jannik0308 <jnkarger01@gmail.com>
2021-07-02 18:11:03 +0200

    Added some Tablist Utils

---
+++ src/main/java/de/jannik0308/minefactprogressmod/utils/ProgressUtils.java
@@ -0,0 +1,44 @@
+package de.jannik0308.minefactprogressmod.utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.overlay.PlayerTabOverlayGui;
+import net.minecraft.util.StringUtils;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class ProgressUtils {
+
+    public static boolean isOnBTEnet() {
+        if(Minecraft.getInstance().isSingleplayer()) return false;
+
+
+        return getTablistHeader() != null && getTablistHeader().contains("BuildTheEarth.net");
+    }
+
+    public static String getTablistHeader() {
+        //Access private header field
+        PlayerTabOverlayGui tablist = Minecraft.getInstance().ingameGUI.getTabList();
+        Field f = ObfuscationReflectionHelper.findField(PlayerTabOverlayGui.class, "field_175256_i");
+        try {
+            ITextComponent text = (ITextComponent) f.get(tablist);
+            List<ITextComponent> siblings = text.getSiblings();
+
+            return siblings.get(0).getString();
+        } catch (IllegalAccessException e) {
+            return null;
+        }
+    }
+
+    public static String getConnectedServer() {
+        if(getTablistHeader() == null || !isOnBTEnet()) return "";
+
+        String connectedServer = getTablistHeader().replace(" ", "")
+                                                   .replace("BuildTheEarth.net", "");
+        connectedServer = StringUtils.stripControlCodes(connectedServer);
+        return connectedServer.split("\\|")[0];
+    }
+
+}
