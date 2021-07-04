bfa5eba097e886692e49ec620bf2fca8ce28f8d6
Jannik0308 <jnkarger01@gmail.com>
2021-07-04 18:42:34 +0200

    Automatic Gamemode/Speed Change on NYC Join

---
+++ src/main/java/de/jannik0308/minefactprogressmod/events/LoggedIn.java
@@ -0,0 +1,26 @@
+package de.jannik0308.minefactprogressmod.events;
+
+import de.jannik0308.minefactprogressmod.config.SettingsConfig;
+import de.jannik0308.minefactprogressmod.utils.ProgressUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class LoggedIn {
+
+    @SubscribeEvent
+    public void onJoin(ClientPlayerNetworkEvent.LoggedInEvent e) {
+        if(Minecraft.getInstance().player != null) {
+            ClientPlayerEntity p = Minecraft.getInstance().player;
+            if(ProgressUtils.getConnectedBTEServer().equals("NewYorkCity")) {
+                if(SettingsConfig.enableAutoGamemode.get()) {
+                    p.sendChatMessage("/gamemode creative");
+                }
+                if(SettingsConfig.enableAutoSpeed.get()) {
+                    p.sendChatMessage("/speed 10");
+                }
+            }
+        }
+    }
+}
